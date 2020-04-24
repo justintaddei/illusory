efc45ad6a9455afde574fb6e10921cea41a98c37
Justin Taddei <justin@justintaddei.com>
2020-04-23 20:23:36 -0500

    Use let instead of var

--- src/polyfill/NodeList.forEach.ts
+++ src/polyfill/NodeList.forEach.ts
@@ -2,7 +2,7 @@
 if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = function(callback, thisArg) {
     thisArg = thisArg || window
-    for (var i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
       callback.call(thisArg, this[i], i, this)
     }
   }
