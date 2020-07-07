444562d0404a908f5bb80dc75fa6d96f18d5304a
Justin Taddei <justin@justintaddei.com>
2020-07-07 14:59:02 -0500

    Fixed typo on internal method Changed seperate to separate Removed unused export

--- src/parsers/parseRGBA.ts
+++ src/parsers/parseRGBA.ts
@@ -3,13 +3,13 @@ const isHex = (hex: string) => /^#([a-f0-9]{4}){1,2}$/i.test(hex)
 
 const hexChunkSize = (hex: string) => Math.floor((hex.length - 1) / 3)
 
-const seperateHex = (hex: string) => hex.match(new RegExp(`.{${hexChunkSize(hex)}}`, 'g'))
+const separateHex = (hex: string) => hex.match(new RegExp(`.{${hexChunkSize(hex)}}`, 'g'))
 
 const hexToDec = (hex: string) => parseInt(hex.repeat(2 / hex.length), 16)
 
 const getAlphaFloat = (a: number) => +(a / 256).toFixed(2)
 
-export const hexToRGBA = (hexArr: string[]) => {
+const hexToRGBA = (hexArr: string[]) => {
   const [r, g, b, a] = hexArr.map(hexToDec)
 
   return [r, g, b, getAlphaFloat(a)]
@@ -33,7 +33,7 @@ export function parseRGBA(color: string): IRGBADescriptor | false {
   if (rgba) {
     ;[r, g, b, a] = rgba.slice(1, 5).map(parseFloat)
   } else if (isHex(color)) {
-    const hexArray = seperateHex(color.slice(1))
+    const hexArray = separateHex(color.slice(1))
 
     if (!hexArray) return false
     ;[r, g, b, a] = hexToRGBA(hexArray)
