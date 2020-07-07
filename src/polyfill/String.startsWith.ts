aed03e579407ead18506c05dbb2fb1d5eebe059a
Justin Taddei <justin@justintaddei.com>
2020-07-07 14:56:37 -0500

    Added startsWith polyfill

---
+++ src/polyfill/String.startsWith.ts
@@ -0,0 +1,9 @@
+// From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
+if (!String.prototype.startsWith) {
+  Object.defineProperty(String.prototype, 'startsWith', {
+    value: function(search: string, rawPos: number) {
+      var pos = rawPos > 0 ? rawPos | 0 : 0
+      return this.substring(pos, pos + search.length) === search
+    }
+  })
+}
