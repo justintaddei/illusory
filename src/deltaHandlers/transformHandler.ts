56c08be67ee2b4a9787532a7cf627b9dec3a8914
Justin Taddei <justin@justintaddei.com>
2020-07-07 19:04:23 -0500

    Added check for malformed matrix

--- src/deltaHandlers/transformHandler.ts
+++ src/deltaHandlers/transformHandler.ts
@@ -6,10 +6,20 @@ const IDENTITY_MATRIX = identity()
 export default function transformHandler(delta: IDelta, deltaStyle: string) {
   let originalMatrixOfDeltaElement: Matrix3D
 
-  if (!deltaStyle.startsWith('matrix')) originalMatrixOfDeltaElement = IDENTITY_MATRIX
-  else originalMatrixOfDeltaElement = fromString(deltaStyle)
+  if (deltaStyle.startsWith('matrix')) originalMatrixOfDeltaElement = fromString(deltaStyle)
+  else originalMatrixOfDeltaElement = IDENTITY_MATRIX
 
   const deltaMatrix = [translate(delta.x, delta.y), originalMatrixOfDeltaElement, scale(delta.scaleX, delta.scaleY)]
 
-  return toString(deltaMatrix.reduce(multiply))
+  let transformString: string
+
+  try {
+    transformString = toString(deltaMatrix.reduce(multiply))
+  } catch (e) {
+    if (process && process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test')
+      console.error('[illusory] Failed to construct transform matrix. Is the element in the DOM?')
+    return 'none'
+  }
+
+  return transformString
 }
